ec365039cb1cb4e6b939a690e8194fa317d4fa84
Hyeokbin-Kwon <hbkwon@tangunsoft.com>
2025-08-04 17:10:18 +0900

    폰트 깨짐 수정

--- src/main.c
+++ src/main.c
@@ -1,13 +1,26 @@
 #include "../include/main.h"
+#include <locale.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 // 전역 변수
 sqlite3 *g_database = NULL;
 SystemConfig g_config;
 
 int main(int argc, char *argv[]) {
+    // Windows 콘솔 UTF-8 설정
+#ifdef _WIN32
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    
+    // 로케일 설정
+    setlocale(LC_ALL, "ko_KR.UTF-8");
+    
     printf("=================================================\n");
     printf("        도서관 관리 시스템\n");
-    printf("        Library Management System\n");
+    printf("        Library Management System\n");  
     printf("=================================================\n");
     
     // 애플리케이션 초기화
